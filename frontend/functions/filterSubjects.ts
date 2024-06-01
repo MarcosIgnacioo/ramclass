29986f9fb77714240d5d2e0e016cf14723360678
MarcosIgnacioo <magma_ruby145@hotmail.com>
2024-06-01 13:00:22 -0700

    fix pocho a lo de filtrar las optativas me da hueva hacer refactor en varios componentes para cambiar la manera en la q hago las cosas, si hay una carrera que tiene 10 semestres el decimo mostrara las optativas nomas alvvv

--- frontend/functions/filterSubjects.ts
+++ frontend/functions/filterSubjects.ts
@@ -1,8 +1,14 @@
 import SubjectClass from "../classes/Subject"
 
 export const filterSubjects = (subjects: Object[], subjectName: string, semester: number) => {
+ // Fix pocho
+ if (semester === 10) {
+  return subjects.filter(subject => ((subject as SubjectClass).semester === 0))
+ }
  subjects = subjects.filter(subject => (((subject as SubjectClass).subject_name.toLowerCase()).includes(subjectName.toLowerCase())))
  subjects = (semester === 0) ? subjects : subjects.filter(subject => (((subject as SubjectClass).semester) === semester))
+ console.log(semester === 10)
+ console.log(semester)
  return subjects
 }
 
