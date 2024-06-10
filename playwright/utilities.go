839901d32bf5cb40d01b0737150bdfa885a40180
MarcosIgnacioo <magma_ruby145@hotmail.com>
2024-06-10 16:10:59 -0700

    Prod build

--- playwright/utilities.go
+++ playwright/utilities.go
@@ -17,7 +17,7 @@ import (
 )
 
 // Variables que sirven para realizar assertions en el momento del scrapping
-var expect = playwright.NewPlaywrightAssertions(750)
+var expect = playwright.NewPlaywrightAssertions(1500)
 var loginSiiaExpect = playwright.NewPlaywrightAssertions(1000)
 var await = playwright.NewPlaywrightAssertions(500)
 
